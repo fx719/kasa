4705bcb663624893c3b88c9ac6ef57891fd7fdc6
fx719 <frederic.dupont78@yahoo.fr>
2026-04-01 18:34:56 +0200

    style (global and Header) : add style to the header section + import Montserrat font.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from 'react-router-dom'
+import logo from '../../assets/images/LOGO.svg'
 import './Header.scss'
 
 function Header() {
     return (
         <>
             <header>
+                <img src={logo} alt="Logo de l'entreprise Kasa" />
                 <div className='navbar'>
                     <NavLink to="/" className='navbar__link'>Accueil</NavLink>
                     <NavLink to="/about" className='navbar__link'>A propos</NavLink>
