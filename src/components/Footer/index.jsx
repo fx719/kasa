069c094a535650bc9ddc241fa76151bf37e8e45e
fx719 <frederic.dupont78@yahoo.fr>
2026-04-08 18:29:09 +0200

    feat(components): add Footer component and add style to it.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
+import footerLogo from '../../assets/images/footer/footerLogo.svg'
 import './Footer.scss'
 
 function Footer() {
     return (<>
-        <footer>
-            <h3>Pied de page</h3>
+        <footer className='footer'>
+            <img src={footerLogo} alt="logo noirci de Kasa" className='footer__logo' />
+            <p className='footer__text'>© 2020 Kasa. All Rights reserved</p>
         </footer>
     </>)
 }
