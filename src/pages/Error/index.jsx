9a952785af88c013642eaa7af0ab2ed14e9a8d9b
fx719 <frederic.dupont78@yahoo.fr>
2026-04-17 08:34:05 +0200

    style(page) : add style to the 404 Error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,14 @@
 import './Error.scss'
+import { NavLink } from 'react-router-dom'
 
 function Error() {
-    return <h1>404 Not Found</h1>
+    return (
+        <div className='notFoundError'>
+            <h1 className='notFoundError__title'>404</h1>
+            <p className='notFoundError__text'>Oups! La page que vous demandez n'existe pas.</p>
+            <NavLink className='notFoundError__link' to='/'>Retourner sur la page d'accueil</NavLink>
+        </div>
+    )
 }
 
 export default Error
