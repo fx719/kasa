cc2d2aa6266069f8e5996c7c5d7ab7431dd2c08e
fx719 <frederic.dupont78@yahoo.fr>
2026-04-20 18:58:55 +0200

    style(components) : add css transition to the Collapse component.

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,14 +1,28 @@
 import './Collapse.scss'
+import { useState, useEffect } from 'react'
 
 function Collapse({ collapseTitle, collapseContent }) {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleCollapse = (e) => {
+        setIsOpen(!isOpen)
+        if (isOpen === false) {
+            e.currentTarget.classList.replace("collapse__header__arrow", "collapse__header__arrow--opening")
+            e.currentTarget.classList.replace("collapse__header__arrow--closing", "collapse__header__arrow--opening")
+        }
+        if (isOpen === true) {
+            e.currentTarget.classList.replace("collapse__header__arrow--opening", "collapse__header__arrow--closing")
+        }
+    }
+
+
     return (<div className='collapse'>
         <div className="collapse__header">
             <h2 className='collapse__header__title'>{collapseTitle}</h2>
-            <i className="collapse__header__arrow fa-solid fa-angle-up fa-2xl" style={{ color: "rgb(255, 255, 255)" }}></i>
+            <i className="fa-solid fa-angle-up fa-2xl collapse__header__arrow" onClick={(e) => handleCollapse(e)}></i>
         </div>
-        <p className='collapse__content'>{collapseContent}</p>
-
-
+        <p className='collapse__content' style={isOpen ? { display: "block" } : { display: "none" }} >{collapseContent}</p>
     </div>)
 }
 
