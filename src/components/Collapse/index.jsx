8cde7036655f67be6d579116652870c283df7cb4
fx719 <frederic.dupont78@yahoo.fr>
2026-04-22 17:21:45 +0200

    feat(component) : add menu's size 's option prop and content-type props to the Collapse Component. Also attach JSDOC to this component.

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,13 +1,30 @@
 import './Collapse.scss'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
-function Collapse({ collapseTitle, collapseContent }) {
+/**
+ * Collapse component that displays content when clicked, and hides it again when clicked a second time.
+ * The arrow is animated with a simple half-complete rotatation.
+ * 
+ * @component
+ * @param {Object} props
+ * @param {boolean} props.isMediumSized Normal width is 75% of parent element, medium width is 30%.
+ * @param {boolean} props.contentIsParagraph
+ * @param {boolean} props.contentIsList
+ * @param {string} props.collapseTitle
+ * @param {string | string[]} props.collapseContent   If contentIsParagraph, then content type is a string. 
+ * If contentIsList, then it's  a strings' array that you have to map on, to display each <li></li> corresponding element.
+ * @returns {JSX.Element}
+ */
 
+function Collapse({ isMediumSized, contentIsParagraph, contentIsList, collapseTitle, collapseContent }) {
+
+    //Collapse menu is closed on page's loading
     const [isOpen, setIsOpen] = useState(false)
 
     const handleCollapse = (e) => {
         setIsOpen(!isOpen)
         if (isOpen === false) {
+            // if the replaced class doesn't exit, the classList.replace() function doesn't throw an error, so here it handles the 2 possible closed-Collapse classes
             e.currentTarget.classList.replace("collapse__header__arrow", "collapse__header__arrow--opening")
             e.currentTarget.classList.replace("collapse__header__arrow--closing", "collapse__header__arrow--opening")
         }
@@ -17,13 +34,16 @@ function Collapse({ collapseTitle, collapseContent }) {
     }
 
 
-    return (<div className='collapse'>
-        <div className="collapse__header">
-            <h2 className='collapse__header__title'>{collapseTitle}</h2>
-            <i className="fa-solid fa-angle-up fa-2xl collapse__header__arrow" onClick={(e) => handleCollapse(e)}></i>
+    return (
+        <div className='collapse' style={isMediumSized && { width: "30%" }}>
+            <div className="collapse__header">
+                <h2 className='collapse__header__title'>{collapseTitle}</h2>
+                <i className="fa-solid fa-angle-up fa-2xl collapse__header__arrow" onClick={(e) => handleCollapse(e)}></i>
+            </div>
+            {contentIsParagraph && <p className='collapse__content--paragraph' style={isOpen ? { display: "block" } : { display: "none" }} >{collapseContent}</p>}
+            {contentIsList && <ul className='collapse__content--list' style={isOpen ? { display: "block" } : { display: "none" }}>{collapseContent}</ul>}
         </div>
-        <p className='collapse__content' style={isOpen ? { display: "block" } : { display: "none" }} >{collapseContent}</p>
-    </div>)
+    )
 }
 
 
--- src/pages/Accomodation/index.jsx
+++ src/pages/Accomodation/index.jsx
@@ -1,6 +1,7 @@
 import accomodations from '../../assets/data/logements.json'
 import { useParams, Navigate } from 'react-router-dom'
 import './Accomodation.scss'
+import Collapse from '../../components/Collapse/index.jsx'
 
 function Accomodation() {
     try {
@@ -23,18 +24,25 @@ function Accomodation() {
                 <div className="visitedAccomodation__introduction">
                     <h1 className='visitedAccomodation__title' >{visitedAccomodation.title}</h1>
                     <p className='visitedAccomodation__location'>{visitedAccomodation.location}</p>
-                    <div className="visitedAccomodation__tags">
-                        {accomodationTags.map((tag) => (
-                            <p key={accomodationElementId++} className='visitedAccomodation__tags__tag'>{tag}</p>
-                        ))}
-                    </div>
-                    <div className="visitedAccomodation__host">
-                        <p className='visitedAccomodation__host__name'>{visitedAccomodation.host.name}</p>
-                        <img className='visitedAccomodation_host__avatar' src={visitedAccomodation.host.picture} alt={`avatar de ${visitedAccomodation.host.name}`} />
-                    </div>
-                    <div className="visitedAccomodation__rating">
-                        <p>Insérer nombre d'étoiles correspondant à {visitedAccomodation.rating}</p>
-                    </div>
+                </div>
+                <div className="visitedAccomodation__tags">
+                    {accomodationTags.map((tag) => (
+                        <p key={accomodationElementId++} className='visitedAccomodation__tags__tag'>{tag}</p>
+                    ))}
+                </div>
+
+                <div className="visitedAccomodation__host">
+                    <p className='visitedAccomodation__host__name'>{visitedAccomodation.host.name}</p>
+                    <img className='visitedAccomodation_host__avatar' src={visitedAccomodation.host.picture} alt={`avatar de ${visitedAccomodation.host.name}`} />
+                </div>
+                <div className="visitedAccomodation__rating">
+                    <p>Insérer nombre d'étoiles correspondant à {visitedAccomodation.rating}</p>
+                </div>
+                <div className="visitedAccomodation__description">
+                    <Collapse isMediumSized contentIsParagraph collapseTitle="Description" collapseContent={visitedAccomodation.description} />
+                </div>
+                <div className="visitedAccomodation__equipment">
+                    <Collapse isMediumSized contentIsList collapseTitle="Equipements" collapseContent={visitedAccomodation.equipments.map((equipment) => (<li key={accomodationElementId++}>{equipment}</li>))} />
                 </div>
             </div>)
         }
